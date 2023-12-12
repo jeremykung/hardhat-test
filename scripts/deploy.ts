96085d5a2c7d5bca6dd11b715e3ddc8ede79a562
es <es@ess-MacBook-Air.local>
2023-12-13 01:15:34 +0200

    added HelloWorld contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const HelloWorld = await ethers.getContractFactory("HelloWorld");
+  const helloWorld = await HelloWorld.deploy();
+  const deployedCode = await helloWorld.getDeployedCode();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log("helloWorld contract deployed:", helloWorld);
+  console.log("deployed code", deployedCode);
+  console.log(await helloWorld.hi());
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // console.log(`deployed helloWorld: ${helloWorld}`);
+  // console.log(`deployed to ${helloWorld.getAddress()}`);
+  // console.log(`function called: ${helloWorld.hi()}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
